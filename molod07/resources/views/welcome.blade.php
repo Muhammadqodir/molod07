9ea66b6538b264bfd27e12d0390511bae7f46213
Muhammadqodir <m.qodir777@gmail.com>
2025-08-02 11:25:38 +0500

    Project created

---
+++ molod07/resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!-- resources/views/welcome.blade.php -->
+<head>
+    @vite('resources/css/app.css')
+    @livewireStyles
+</head>
+<body>
+    <h1 class="text-2xl font-bold text-center mt-10">Привет, Laravel + Tailwind!</h1>
+
+    @vite('resources/js/app.js')
+    @livewireScripts
+</body>
+</html>
