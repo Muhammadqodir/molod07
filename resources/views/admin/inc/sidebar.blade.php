e2bdd9be06ef03afe515ae7dab9d9631095d9c85
Muhammadqodir <m.qodir777@gmail.com>
2025-11-06 23:33:19 +0500

    Added other option to event type

--- resources/views/admin/inc/sidebar.blade.php
+++ resources/views/admin/inc/sidebar.blade.php
@@ -2,7 +2,6 @@
     {{-- простые пункты --}}
     <x-sidebar-link route="admin.profile" icon="user-circle" label="Личные данные" />
     <x-sidebar-link route="admin.feedback.index" icon="message-circle" label="Обратная связь" />
-    <x-sidebar-link route="admin.support" icon="headphones" label="Поддержка" />
 
     {{-- Группа: Управление --}}
     <x-sidebar-group icon="user-cog" label="Управление" :open="request()->routeIs('admin.manage.*')">
